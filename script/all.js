deadca439d6d3abac9f73a810562a974cc0ca156
sky030b <sky030blue030@gmail.com>
2023-11-19 19:33:39 +0800

    lastest-event update

--- script/all.js
+++ script/all.js
@@ -1,7 +1,6 @@
 const events = [
   {
     date: "2023/12/24",
-    color: "red",
     image: "./image/cover1.png",
     title: "掃街模式開啟！帶著你的貓耳，來和我一起走！",
     descriptionS: "街上氣氛真的很棒，從小孩到大人，甚至有些狗狗朋友都帶著貓耳來找我握手，真的太可愛了！",
@@ -9,7 +8,6 @@ const events = [
   },
   {
     date: "2023/12/20",
-    color: "blue",
     image: "./image/cover2.png",
     title: "收容所模特兒大比拼！",
     descriptionS: "今天的收容所不再是一片寂靜。為了讓更多人認識到這裡的毛孩子，我們舉辦了一場前所未有的「模特兒走秀」！",
@@ -17,7 +15,6 @@ const events = [
   },
   {
     date: "2023/12/26",
-    color: "green",
     image: "./image/cover.png",
     title: "參與台北寵物論壇，爭取貓咪友善環境",
     descriptionS: "炎炎夏日的周六，我走進了台北寵物論壇，帶著一副貓耳髮箍，決定要全力宣傳「貓咪至上」的理念！",
@@ -89,7 +86,6 @@ function renderFocus(data) {
   const bgi = document.querySelector('.bgi');
   console.log(bgi, data.image)
   bgi.style.backgroundImage = `url(${data.image})`
-  // console.log(123)
 }
 renderFocus(events[1]);
 
